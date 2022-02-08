76de226533a60f46326eaca283c545d907dab487
jhonnferrerosa <jhonn.trucke@gmail.com>
2022-02-08 13:33:41 +0100

    Add files via upload

---
+++ test2prueba.py
@@ -0,0 +1,8 @@
+print ("este va a ser el segundo archivo que tenemos de python");
+
+x = int(input("escriba el numero de repeticiones que quiera:   "));
+aux = 0;
+
+while (aux < x):
+	print ("este es tu numero:  ", aux);
+	aux = aux + 1;
