3fbf2a436be9f1dc376a6723480a77cedc5b8193
jhonnferrerosa <jhonn.trucke@gmail.com>
2022-08-15 13:31:08 +0200

    Add files via upload

---
+++ contarCaracteres.py
@@ -0,0 +1,16 @@
+##este programa va a ser para contar caracteres
+
+
+print ("--- este programa va a ser para contar caracteres ---");
+
+caracteres = "";
+cantidadCaracteres = 0;
+
+caracteres = str(input("escribe la cadena aqui:  "));
+cantidadCaracteres =  len (caracteres);
+
+
+
+
+print ("esta es la cantidad de caracteres:  ", cantidadCaracteres);
+
---
+++ test.py
@@ -0,0 +1,6 @@
+print  ("hola  a todos");
+
+
+num = [1,2,3,4,5];
+num.remove(2);
+print (num);
