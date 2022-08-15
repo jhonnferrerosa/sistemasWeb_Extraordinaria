64e23074f6ba48857bf0af0136c3dc158a5ac6ff
jhonnferrerosa <jhonn.trucke@gmail.com>
2022-08-15 13:30:34 +0200

    Delete test2prueba.py

--- test2prueba.py
+++
@@ -1,8 +0,0 @@
-print ("este va a ser el segundo archivo que tenemos de python");
-
-x = int(input("escriba el numero de repeticiones que quiera:   "));
-aux = 0;
-
-while (aux < x):
-	print ("este es tu numero:  ", aux);
-	aux = aux + 1;
